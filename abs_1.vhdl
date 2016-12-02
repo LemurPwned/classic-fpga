6992082c31cf5283359a7cdb42e2f4d5880e1d6e
Jakub Mojsiejuk <jakub.mojsiejuk@gmail.com>
2016-12-02 15:24:11 +0100

    Added u2/u1 decoder priority selection on incoming signal

--- abs_1.vhdl
+++ abs_1.vhdl
@@ -5,27 +5,59 @@ use ieee.numeric_std.all;
 entity abs_1 is
   port (
   si : in std_logic_vector(4 downto 0);
-  smo : out std_logic_vector(3 downto 0)
+  smo : out std_logic_vector(4 downto 0);
+  se : in std_logic_vector (0 downto 0);
+  s : in std_logic_vector (0 downto 0)
   );
 end entity;
 
 architecture behav of abs_1 is
   signal nsi : std_logic_vector(4 downto 0);
-  signal sum : std_logic_vector(3 downto 0);
+  signal sum, sum2 : std_logic_vector(4 downto 0);
   signal c : std_logic := '1';
   signal t,f,g : std_logic := '0';
-begin
-  nsi <= not si;
+  signal nse : std_logic_vector(0 downto 0);
+  signal smo1, smo2 : std_logic_vector(4 downto 0);
 
-    c <='1';
-    sum(0) <= nsi(0) xor c;
-    t <= nsi(0) and c;
-    sum(1) <= nsi(1) xor t;
-    f <= nsi(1) and t;
-    sum(2) <= nsi(2) xor f;
-    g <= nsi(2) and f;
-    sum(3) <= nsi(3) xor g;
-    smo <= sum;
+begin
+  -- s = 1 for convert
+  -- se = 1 select u1
+  nsi <= not si;
+  nse <= not se;
+  sum(0) <= si(0) xor s(0);
+  sum(1) <= si(1) xor s(0);
+  sum(2) <= si(2) xor s(0);
+  sum(3) <= si(3) xor s(0);
+  sum(4) <= si(4) xor s(0);
+
+  sum2(0) <= nsi(0) xor (not se(0));
+  sum2(1) <= nsi(1) xor nsi(0);
+  sum2(2) <= nsi(2) xor (nsi(0) and nsi(1));
+  sum2(3) <= nsi(3) xor (nsi(0) and nsi(1) and nsi(2));
+  sum2(4) <= nsi(4) xor (nsi(0) and nsi(1) and nsi(2) and nsi(3));
+
+  smo1(0) <= se(0) and sum(0);
+  smo1(1) <= se(0) and sum(1);
+  smo1(2) <= se(0) and sum(2);
+  smo1(3) <= se(0) and sum(3);
+  smo1(4) <= se(0) and sum(4);
+
+  smo2(0) <= nse(0) and sum2(0);
+  smo2(1) <= nse(0) and sum2(1);
+  smo2(2) <= nse(0) and sum2(2);
+  smo2(3) <= nse(0) and sum2(3);
+  smo2(4) <= nse(0) and sum2(4);
+
+  smo <= smo1 or smo2;
+    --c <='1';
+    --sum(0) <= nsi(0) xor c;
+    --t <= nsi(0) and c;
+    --sum(1) <= nsi(1) xor t;
+    --f <= nsi(1) and t;
+    --sum(2) <= nsi(2) xor f;
+    --g <= nsi(2) and f;
+    --sum(3) <= nsi(3) xor g;
+    --smo <= sum;
 
         ----only U2 for now
     --sum(0) <= nsi(0) xor c;
--- abs_1_tb.vhdl
+++ abs_1_tb.vhdl
@@ -10,26 +10,48 @@ architecture behav of abs_1_tb is
     component abs_1
     port (
     si : in std_logic_vector(4 downto 0);
-    smo : out std_logic_vector(3 downto 0)
+    smo : out std_logic_vector(4 downto 0);
+    se : in std_logic_vector(0 downto 0);
+    s : in std_logic_vector(0 downto 0)
     );
   end component;
 
 for abs_1_0 : abs_1 use entity work.abs_1;
 
 signal si : std_logic_vector(4 downto 0) :="00000";
-signal smo: std_logic_vector(3 downto 0);
+signal smo: std_logic_vector(4 downto 0);
+signal se, s :std_logic_vector (0 downto 0);
 
 
 begin
-  abs_1_0 : abs_1 port map (si=>si, smo=>smo);
+  abs_1_0 : abs_1 port map (si=>si, smo=>smo, se=>se, s=>s);
   process
     begin
       si<="10011";
+      se<="1";
+      s<="1";
       wait for 5 ns;
-      si<="00111";
+
+      si<="10011";
+      se<="0";
+      s<="1";
       wait for 5 ns;
-      si<="10111";
+
+      si<="10000";
+      se<="0";
+      s<="1";
       wait for 5 ns;
+
+      si<="11011";
+      se<="0";
+      s<="1";
+      wait for 5 ns;
+
+      si<="11011";
+      se<="1";
+      s<="0";
+      wait for 5 ns;
+
       wait;
     end process;
 end architecture;
